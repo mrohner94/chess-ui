91332134d734c6b2c2b1257482d4922d4c846f03
mrohner94 <87048324+mrohner94@users.noreply.github.com>
2023-06-08 17:12:41 -0400

    Update app.ts

--- src/store/app.ts
+++ src/store/app.ts
@@ -3,15 +3,11 @@ import { defineStore } from "pinia";
 
 interface State {
   showNavbar: boolean;
-  navMinimized: boolean;
-  entity: "TZI" | "TZA" | "TZC";
   highlightedSquares: Square[];
 }
 export const useAppStore = defineStore("app", {
   state: (): State => ({
     showNavbar: false,
-    navMinimized: false,
-    entity: "TZI",
     highlightedSquares: [],
   }),
   getters: {
